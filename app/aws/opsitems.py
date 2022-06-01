ee7a37009209c74831765eb33266853fea379aa0
Paul Campbell <pcampbell@kemitix.net>
2022-06-01 13:38:19 +0100

    disable admin page navigation

--- app/aws/opsitems.py
+++ app/aws/opsitems.py
@@ -10,22 +10,22 @@ def aws_opsitems(driver):
     @print_timing("selenium_aws_admin_projects_page")
     def navigate_to_admin_projects_page():
         navigator.admin_projects_page()
-    navigate_to_admin_projects_page()
+    #navigate_to_admin_projects_page()
 
     @print_timing("selenium_aws_manage_apps_page")
     def navigate_to_manage_apps_page():
         navigator.manage_apps_page()
-    navigate_to_manage_apps_page()
+    #navigate_to_manage_apps_page()
 
     @print_timing("selenium_aws_account_list_page")
     def navigate_to_account_list_page():
         navigator.account_list_page()
-    navigate_to_account_list_page()
+    #navigate_to_account_list_page()
 
     @print_timing("selenium_aws_connector_settings_page")
     def navigate_to_connector_setting_page():
         navigator.connector_settings_page()
-    navigate_to_connector_setting_page()
+    #navigate_to_connector_setting_page()
 
     @print_timing("selenium_aws_opsitem_create_and_resolve")
     def opsitems_create_and_resolve():
