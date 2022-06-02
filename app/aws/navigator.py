9da6cb329ea5e12a335bee84426ad8e83c027138
Paul Campbell <pcampbell@kemitix.net>
2022-06-02 09:18:29 +0100

    Set description

--- app/aws/navigator.py
+++ app/aws/navigator.py
@@ -11,7 +11,8 @@ from selenium_ui.conftest import print_timing
 from selenium_ui.jira.pages.pages import Issue, Project
 from selenium_ui.jira.pages.selectors import IssueLocators
 from selenium_ui.jsm.pages.agent_pages import Login
-from util.conf import BaseAppSettings
+from util.api.jira_clients import JiraRestClient
+from util.conf import BaseAppSettings, JIRA_SETTINGS
 from .pages.accountDetailsPage import AccountDetailsPage
 from .pages.accountListPage import AccountListPage
 from .pages.adminProjectsPage import AdminProjectsPage, ProjectNotFoundError
@@ -169,6 +170,9 @@ class Navigator:
                         severity: str,
                         category: str,
                         region: str) -> str:
+        client = JiraRestClient(JIRA_SETTINGS.server_url, JIRA_SETTINGS.admin_login, JIRA_SETTINGS.admin_password)
+        rte_status = client.check_rte_status()
+
         @print_timing("load_aws_project")
         def load_aws_project():
             project_page = ProjectPage(self.driver, project_key=project)
@@ -188,16 +192,26 @@ class Navigator:
                 if issue_dropdown_elements:
                     for element in issue_dropdown_elements:
                         cls = element.get_attribute('class')
-                        print(f'cls: {cls}')
                         if 'aui-list-item-li-aws-opsitem' in cls:
-                            issue_modal.action_chains().move_to_element(element).click(element).perform()
+                            issue_modal.action_chains().move_to_element(
+                                element).click(element).perform()
                 issue_modal.wait_until_invisible(IssueLocators.issue_ready_to_save_spinner)
             select_aws_opsitem_issue_type()
 
-            @print_timing("set aws_opsitem_summary")
+            @print_timing("set_aws_opsitem_summary")
             def set_aws_opsitem_summary():
                 issue_modal.wait_until_clickable(IssueLocators.issue_summary_field).send_keys(summary)
             set_aws_opsitem_summary()
+
+            @print_timing("set_aws_opsitem_description")
+            def set_aws_opsitem_description():
+                if rte_status:
+                    issue_modal.wait_until_available_to_switch(IssueLocators.issue_description_field_RTE)
+                    issue_modal.get_element(IssueLocators.tinymce_description_field).send_keys(description)
+                    issue_modal.return_to_parent_frame()
+                else:
+                    issue_modal.get_element(IssueLocators.issue_description_field).send_keys(description)
+            set_aws_opsitem_description()
 
             time.sleep(20)
 
