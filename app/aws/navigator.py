b812d18531d385a85d1714314c482eba40b5c9d1
Paul Campbell <pcampbell@kemitix.net>
2022-06-02 10:16:31 +0100

    Remove deadwood from navigator

--- app/aws/navigator.py
+++ app/aws/navigator.py
@@ -1,176 +1,27 @@
-import json
-import time
 from typing import List
 
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 
 from selenium_ui.conftest import print_timing
-from selenium_ui.jira.pages.pages import Issue, Project
+from selenium_ui.jira.pages.pages import Issue
 from selenium_ui.jira.pages.selectors import IssueLocators
-from selenium_ui.jsm.pages.agent_pages import Login
 from util.api.jira_clients import JiraRestClient
-from util.conf import BaseAppSettings, JIRA_SETTINGS
-from .pages.accountDetailsPage import AccountDetailsPage
-from .pages.accountListPage import AccountListPage
-from .pages.adminProjectsPage import AdminProjectsPage, ProjectNotFoundError
-from .pages.connectAccountPage import AccountDetails
-from .pages.connectorSettingsPage import ConnectorSettingsPage
-from .pages.createIssueFirstPage import CreateIssueFirstPage
-from .pages.loggedInPage import LoggedInPage
-from .pages.manageAppsPage import ManageAppsPage
-from .pages.pageElements import PageElement, Locator
+from util.conf import JIRA_SETTINGS
 from .pages.projectPage import ProjectPage
-from .pages.viewIssuePage import ViewIssuePage
 
 
 class Navigator:
 
     driver: WebDriver
-    logged_in: bool
-    has_account: bool
-    last_page: str
 
-    account_details = AccountDetails(
-        alias = "cloudsoftqa",
-        sync_key = "",  # os.environ['AWS_QA_SYNC_KEY'],
-        sync_secret = "",  # os.environ['AWS_QA_SYNC_SECRET'],
-        end_key = "",  # os.environ['AWS_QA_END_KEY'],
-        end_secret = "",  # os.environ['AWS_QA_END_SECRET']
-    )
-
-    def __init__(self, driver, yaml_file):
+    def __init__(self, driver):
         self.driver = driver
-        self.logged_in = False
-        self.has_account = False
-        self.last_page = 'unknown'
-        self.settings = BaseAppSettings(config_yml=yaml_file)
-        self.site_url = self.settings.server_url
-        self.site_user = self.settings.admin_login
-        self.site_password = self.settings.admin_password
-
-    def login(self) -> LoggedInPage:
-        if not self.logged_in:
-            login_page = Login(self.driver)
-            login_page.go_to()
-            self.driver.node_id = login_page.get_node_id()
-            login_page.set_credentials(username=self.site_user, password=self.site_password)
-            if login_page.is_first_login():
-                login_page.first_login_setup()
-            if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
-            login_page.wait_for_page_loaded()
-            self.logged_in = True
-        return LoggedInPage(self.driver)
-
-    def manage_apps_page(self) -> ManageAppsPage:
-        if not self.last_page == 'manage_apps':
-            page = self.login()
-            page.click_admin_menu()
-            page.click_manage_apps()
-            page.confirm_password_if_required(self.site_password)
-            self.last_page = 'manage_apps'
-        return ManageAppsPage(self.driver)
-
-    def account_list_page(self) -> AccountListPage:
-        if not self.last_page == 'account_list':
-            self.manage_apps_page().click_aws_accounts()
-            self.last_page = 'account_list'
-        return AccountListPage(self.driver)
-
-    def account_details_page(self, index: int) -> AccountDetailsPage:
-        if not self.last_page == 'account_details_' + str(index):
-            self.account_list_page().manage_account(index)
-            self.last_page = 'account_details_' + str(index)
-        return AccountDetailsPage(self.driver)
-
-    def connector_settings_page(self) -> ConnectorSettingsPage:
-        if not self.last_page == 'connector_settings':
-            self.manage_apps_page().click_connector_settings()
-            self.last_page = 'connector_settings'
-        return ConnectorSettingsPage(self.driver)
-
-    def connect_account(self) -> AccountDetailsPage:
-        if not self.last_page == 'account_details':
-            account_list_page = self.account_list_page()
-            account_list_page.delete_all()
-            account_list_page.click_add_first_account()
-            account_details_page = account_list_page.create_account(self.account_details)
-            self.has_account = True
-            self.last_page = 'account_details'
-            return account_details_page
-        return AccountDetailsPage(self.driver)
-
-    def connect_account_and_sync(self) -> AccountDetailsPage:
-        account_details_page = self.connect_account()
-        account_details_page.click_sync_now_button()
-        return account_details_page
-
-    def sync_connected_account(self, index: int) -> AccountDetailsPage:
-        account_details_page = self.account_details_page(index)
-        account_details_page.click_sync_now_button()
-        return account_details_page
-
-    def create_project(self, project_name: str, project_key: str) -> ProjectPage:
-        page = self.admin_projects_page()
-        page.click_create_project_button()
-        page.click_create_project_next_button()
-        page.create_project_name = project_name + Keys.TAB
-        page.create_project_key = project_key + Keys.TAB
-        page.click_create_project_submit_button()
-        self.last_page = "project_page_" + project_key
-        return self.project_page(project_key)
-
-    def configure_connector(self, project_key: str):
-        connector_settings_page = self.connector_settings_page()
-        connector_settings_page.disable_all_integrations()
-        connector_settings_page.enable_for_project(project_key)
-        connector_settings_page.configure_opscenter(project_key)
-        connector_settings_page.configure_security_hub()
-        time.sleep(100)
-        # TODO SAVE
-
-    def admin_projects_page(self) -> AdminProjectsPage:
-        print("Admin Project Page - Last Page: " + self.last_page)
-        # secure/project/BrowseProjects.jspa?s=view_projects
-        #if not self.last_page == "admin_projects":
-        page = self.login()
-        page.click_admin_menu()
-        page.click_admin_projects_menu()
-        self.last_page = "admin_projects"
-        return AdminProjectsPage(self.driver)
-
-    def create_issue_page(self):
-        if not self.last_page == "create_issue":
-            self.admin_projects_page().click_toolbar_create_button()
-            self.last_page = "create_issue"
-        return CreateIssueFirstPage(self.driver)
-
-    def project_page(self, project_key: str) -> ProjectPage:
-        if not self.last_page == "project_page_" + project_key:
-            page = self.admin_projects_page()
-            page.find_and_click_project(project_key)
-            self.last_page = "project_page_" + project_key
-        return ProjectPage(self.driver)
-
-    def delete_project_if_exists(self, project_key) -> AdminProjectsPage:
-        try:
-            project_page = self.project_page(project_key)
-            project_page.delete_project()
-            self.last_page = "admin_projects"
-            return self.admin_projects_page()
-        except ProjectNotFoundError:
-            return self.admin_projects_page()
 
     def create_ops_item(self,
                         project: str,
                         summary: str,
-                        description: str,
-                        severity: str,
-                        category: str,
-                        region: str) -> str:
+                        description: str) -> None:
         client = JiraRestClient(JIRA_SETTINGS.server_url, JIRA_SETTINGS.admin_login, JIRA_SETTINGS.admin_password)
         rte_status = client.check_rte_status()
 
@@ -220,44 +71,3 @@ class Navigator:
             submit_aws_opsitem()
 
         load_aws_create_issue_modal()
-
-    def resolve_issue(self, issue_key: str) -> None:
-        issue_page = self.issue_page(issue_key)
-        issue_page.resolve()
-
-    def issue_page(self, issue_key: str):
-        if not self.last_page == "issue_page_" + issue_key:
-            self.get("browse/" + issue_key)
-            self.last_page = "issue_page_" + issue_key
-        return ViewIssuePage(self.driver)
-
-    def get(self, path: str):
-        url = self.site_url + path
-        self.driver.get(url)
-
-    def find_open_ops_item(self, project_key: str) -> str:
-        issue_key = self.search_issue(project_key=project_key, issue_type="AWS OpsItem", status="Open")
-        return issue_key
-
-    def in_progress_issue(self, issue_key: str) -> None:
-        issue_page = self.issue_page(issue_key)
-        issue_page.in_progress()
-
-    def search_issue(self, project_key: str, issue_type: str, status: str) -> str:
-        page = self.issue_search_page()
-        page.filter_project(project_key)
-        page.filter_type(issue_type)
-        page.filter_status(status)
-        # TODO - does this need a delay?
-        return page.issue_key
-
-    def issue_search_page(self) -> ViewIssuePage:
-        if not self.last_page.startswith("issue_search"):
-            page = self.login()
-            page.click_issue_menu()
-            page.click_issue_search_menu()
-            self.last_page = "issue_search"
-        return ViewIssuePage(self.driver)
-
-    def custom_field_locator(self, label: str) -> Locator:
-        return PageElement.by_xpath(f"//label[text()='{label}']/../select")
--- app/aws/opsitems.py
+++ app/aws/opsitems.py
@@ -7,36 +7,14 @@ def aws_opsitems(driver):
 
     navigator = Navigator(driver, JSM_YML)
 
-    @print_timing("selenium_aws_admin_projects_page")
-    def navigate_to_admin_projects_page():
-        navigator.admin_projects_page()
-    #navigate_to_admin_projects_page()
-
-    @print_timing("selenium_aws_manage_apps_page")
-    def navigate_to_manage_apps_page():
-        navigator.manage_apps_page()
-    #navigate_to_manage_apps_page()
-
-    @print_timing("selenium_aws_account_list_page")
-    def navigate_to_account_list_page():
-        navigator.account_list_page()
-    #navigate_to_account_list_page()
-
-    @print_timing("selenium_aws_connector_settings_page")
-    def navigate_to_connector_setting_page():
-        navigator.connector_settings_page()
-    #navigate_to_connector_setting_page()
-
-    @print_timing("selenium_aws_opsitem_create_and_resolve")
-    def opsitems_create_and_resolve():
+    @print_timing("selenium_aws_opsitem")
+    def opsitem():
         """create an ops item issue and resolve it"""
-        #driver.navigate().to('/jira/secure/CreateIssue!default.jspa')
-        issue_key = navigator.create_ops_item(
+        navigator.create_ops_item(
             project="AWS",
             summary="new ops item",
             description="ops item description",
             severity="4 - Low",
             category="Performance",
             region="eu-central-1")
-        #navigator.resolve_issue(issue_key)
-    opsitems_create_and_resolve()
+    opsitem()
